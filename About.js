a04b82763b4b5e7ed78a6ec947f75c049f07fe05
sameerkusahu <83450066+sameerkusahu@users.noreply.github.com>
2021-11-03 13:09:16 +0530

    pages done

---
+++ About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          In ancient manuscripts, another mean to divide sentences into
+          paragraphs was a line break (newline) followed by an initial at the
+          beginning of the next paragraph. An initial is an oversized capital
+          letter, sometimes outdented beyond the margin of the text. This style
+          can be seen, for example, in the original Old English manuscript of
+          Beowulf. Outdenting is still used in English typography, though not
+          commonly.[3] Modern English typography usually indicates a new
+          paragraph by indenting the first line. This style can be seen in the
+          (handwritten) United States Constitution from 1787. For additional
+          ornamentation, a hedera leaf or other symbol can be added to the
+          inter-paragraph white space, or put in the indentation space.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
